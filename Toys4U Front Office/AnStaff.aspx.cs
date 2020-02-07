1c7c6ab73be1634359b39158ab8d054424d00e02
Callum Simpson <P2444354@my365.dmu.ac.uk>
2020-02-07 15:05:25 +0000

    Button Ok event handler created.

--- Toys4U Front Office/AnStaff.aspx.cs
+++ Toys4U Front Office/AnStaff.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Toys4U_Classes;
 
 namespace Toys4U_Front_Office
 {
@@ -16,6 +17,14 @@ namespace Toys4U_Front_Office
 
         protected void ButtonOK_Click(object sender, EventArgs e)
         {
+            //create a new instance of the clsStaff
+            clsStaff AnStaff = new clsStaff();
+            //capture the date joined
+            AnStaff.FirstName = TextBoxFirstName.Text;
+            //store the first name in the session object
+            Session["AnStaff"] = AnStaff;
+            //redurect to the viewer page
+            Response.Redirect("StaffViewer.aspx");
 
         }
     }
