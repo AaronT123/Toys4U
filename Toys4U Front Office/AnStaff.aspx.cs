2086b901150580d6e2581e430b6f43348ffbd345
Callum Simpson <P2444354@my365.dmu.ac.uk>
2020-02-07 14:47:09 +0000

    Added front end web page for staff.

---
+++ Toys4U Front Office/AnStaff.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Toys4U_Front_Office
+{
+    public partial class AnStaff : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonOK_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
