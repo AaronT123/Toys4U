56e4175098479f2232ab519d32abff8e250e45f4
Callum Simpson <P2444354@my365.dmu.ac.uk>
2020-02-07 15:08:56 +0000

    Staff viewer page created. Load event on this page modified to retrive data from session object.

---
+++ Toys4U Front Office/StaffViewer.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Toys4U_Classes;
+namespace Toys4U_Front_Office
+{
+    public partial class StaffViewer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //create a new instance of clsStaff
+            clsStaff AnStaff = new clsStaff();
+            //get the data from the session object
+            AnStaff = (clsStaff)Session["AnStaff"];
+            //display the first name for this entry
+            Response.Write(AnStaff.FirstName);
+        }
+    }
+}
