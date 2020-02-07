ebc356371d4e8c0d7bfeffa7abd98d88c6b8a824
Callum Simpson <P2444354@my365.dmu.ac.uk>
2020-02-07 14:07:52 +0000

    Added first instance and property tests.

--- Toys4U Classes/clsStaff.cs
+++ Toys4U Classes/clsStaff.cs
@@ -1,6 +1,11 @@
-﻿namespace Toys4U_Classes
+﻿using System;
+
+namespace Toys4U_Classes
 {
     public class clsStaff
     {
+        public bool Admin { get; set; }
+        public DateTime DateJoined { get; set; }
+        public DateTime DateOfBirth { get; set; }
     }
 }
--- Toys4U Testing/tstStaff.cs
+++ Toys4U Testing/tstStaff.cs
@@ -7,6 +7,7 @@ namespace Toys4U_Testing
     [TestClass]
     public class tstStaff
     {
+        //test method to check if instance can be created
         [TestMethod]
         public void InstanceOK()
         {
@@ -14,6 +15,56 @@ namespace Toys4U_Testing
             clsStaff AnStaff = new clsStaff();
             //test to see that it exists
             Assert.IsNotNull(AnStaff);
+
+        }
+        //test methods for propertys
+        [TestMethod]
+        public void AdminPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //creaate some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            AnStaff.Admin = TestData;
+            //test to see that the two values are the same 
+            Assert.AreEqual(AnStaff.Admin, TestData);
+
+
+
+        }
+
+
+        [TestMethod]
+        public void DateJoinedPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //creaate some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AnStaff.DateJoined = TestData;
+            //test to see that the two values are the same 
+            Assert.AreEqual(AnStaff.DateJoined, TestData);
+
+
+
+        }
+
+
+        [TestMethod]
+        public void DateOfBirthPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //creaate some test data to assign to the property
+            DateTime TestData = new DateTime(2000, 12, 12);
+            //assign the data to the property
+            AnStaff.DateOfBirth = TestData;
+            //test to see that the two values are the same 
+            Assert.AreEqual(AnStaff.DateOfBirth, TestData);
+
+
 
         }
     }
