48c550db07cd7de72e4a3029e4d1a29dd183e757
P2446237 <P2446237@GWHXX473WIN014W>
2020-02-12 11:45:55 +0000

    test

--- Toys4U Testing/tstCustomer.cs
+++ Toys4U Testing/tstCustomer.cs
@@ -16,5 +16,67 @@ namespace Toys4U_Testing
             Assert.IsNotNull(AnCustomer);
 
         }
+        [TestMethod]
+        public void CustomerIDOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            AnCustomer.CustomerID = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnCustomer.CustomerID, TestData);
+        }
+        [TestMethod]
+        public void EmailAddressOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //create some test data to assign to the property
+            string TestData = "Abdisuldan@gmail.com";
+            //assign the data to the property
+            AnCustomer.EmailAddress = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnCustomer.EmailAddress, TestData);
+        }
+
+        [TestMethod]
+        public void FirstNameOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //create some test data to assign to the property
+            string TestData = "Abdi";
+            //assign the data to the property
+            AnCustomer.FirstName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnCustomer.FirstName, TestData);
+
+        }
+
+        [TestMethod]
+        public void LastNameOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //create some test data to assign to the property
+            string TestData = "Suldan";
+            //assign the data to the property
+            AnCustomer.LastName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnCustomer.LastName, TestData);
+        }
+        [TestMethod]
+        public void PasswordOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //create some test data to assign to the property
+            string TestData = "Abdis";
+        }
+    
+
+
     }
 }
