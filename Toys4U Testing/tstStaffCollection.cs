d1b1321a354af43189f186c62f1e4d669073f771
Callum Simpson <P2444354@my365.dmu.ac.uk>
2020-02-14 18:33:24 +0000

    Created tstStaffCollection

---
+++ Toys4U Testing/tstStaffCollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Toys4U_Testing
+{
+    [TestClass]
+    public class tstStaffCollection
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
