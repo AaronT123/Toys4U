35d366727bd5d136172878a09a5c43977e338e52
Callum Simpson <P2444354@my365.dmu.ac.uk>
2020-02-14 20:29:46 +0000

    test if records can be added to the lists and the Count property correctly reports how many items are in the list.

--- Toys4U Classes/clsStaffCollection.cs
+++ Toys4U Classes/clsStaffCollection.cs
@@ -4,12 +4,43 @@ namespace Toys4U_Classes
 {
     public class clsStaffCollection
     {
+        //private data member for the list
+        List<clsStaff> mStaffList = new List<clsStaff>();
         public clsStaffCollection()
         {
         }
 
-        public List<clsStaff> StaffList { get; set; }
-        public int Count { get; set; }
+        //public List<clsStaff> StaffList { get; set; }
+        //expose the private data via the public property
+        public List<clsStaff> StaffList
+        {
+            get
+            {
+                //return the private data
+                return mStaffList;
+
+            }
+            set
+            {
+                //set the private data
+                mStaffList = value;
+            }
+        }
+        //return the value of the Count property of the list as the value of our own 
+        public int Count
+        {
+            get
+            {
+                //return the count of the list
+                return mStaffList.Count;
+            }
+            set
+            {
+                //we shall worry about this later
+            }
+        }
+
+       
         public clsStaff ThisStaff { get; set; }
     }
 }
--- Toys4U Testing/tstStaffCollection.cs
+++ Toys4U Testing/tstStaffCollection.cs
@@ -74,7 +74,7 @@ namespace Toys4U_Testing
             //create some test data to assign to the property
             clsStaff TestStaff = new clsStaff();
             //set the properties of the test object 
-            //TestItem.DateOfBirth = 01/'01/2000;
+            //TestStaff.DateOfBirth = 01/'01/2000;
             TestStaff.DateJoined = DateTime.Now.Date;
             TestStaff.Email = "callumjohnsimpson@hotmail.com";
             TestStaff.HourlyPay = 7.77m;
@@ -89,5 +89,36 @@ namespace Toys4U_Testing
             Assert.AreEqual(AllStaff.ThisStaff, TestStaff);
         }
 
+        //test if records can be added to the lists
+        //and the Count property correctly reports how many items are in the list.
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //create an instance of the class we want to create
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            //create some test data to asign to the property
+            //in this case the data needs to be a list of objects
+            List<clsStaff> TestList = new List<clsStaff>();
+            //add an item to the list
+            //create the item of test data 
+            clsStaff TestItem = new clsStaff();
+            //set its properties
+            //TestItem.DateOfBirth = 01/'01/2000;
+            TestItem.DateJoined = DateTime.Now.Date;
+            TestItem.Email = "callumjohnsimpson@hotmail.com";
+            TestItem.HourlyPay = 7.77m;
+            TestItem.LastName = "Simpson";
+            TestItem.Password = "Something!";
+            TestItem.PhoneNumber = "07465 659874";
+            TestItem.FirstName = "Callum";
+            TestItem.JobTitle = "Manager";
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllStaff.StaffList = TestList;
+            //test to see that the two values are tghe same
+            Assert.AreEqual(AllStaff.Count, TestList.Count);
         }
+    }
+
 }
