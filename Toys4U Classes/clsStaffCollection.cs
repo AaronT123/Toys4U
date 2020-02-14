a43bd63ab94af912bb9a00dcb2d57ccf653c6771
Callum Simpson <P2444354@my365.dmu.ac.uk>
2020-02-14 21:04:14 +0000

    Editing the clsStaff Constructor to add two records to the list

--- Toys4U Classes/clsStaffCollection.cs
+++ Toys4U Classes/clsStaffCollection.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Toys4U_Classes
 {
@@ -6,8 +7,39 @@ namespace Toys4U_Classes
     {
         //private data member for the list
         List<clsStaff> mStaffList = new List<clsStaff>();
+        
+        //constructor for the class
         public clsStaffCollection()
         {
+            //create the items of test data
+            clsStaff TestItem = new clsStaff();
+            //set its propertiesdd
+            //TestItem.DateOfBirth = 01/'01/2000;
+            TestItem.DateJoined = DateTime.Now.Date;
+            TestItem.Email = "callumjohnsimpson@hotmail.com";
+            TestItem.HourlyPay = 7.77m;
+            TestItem.LastName = "Simpson";
+            TestItem.Password = "Something!";
+            TestItem.PhoneNumber = "07465 659874";
+            TestItem.FirstName = "Callum";
+            TestItem.JobTitle = "Manager";
+            //add the item to the list 
+            mStaffList.Add(TestItem);
+            //re initialise the object for some new data 
+            TestItem = new clsStaff();
+            //set its properties
+            //TestItem.DateOfBirth = 01/'01/2000;
+            TestItem.DateJoined = DateTime.Now.Date;
+            TestItem.Email = "random@hotmail.com";
+            TestItem.HourlyPay = 6.77m;
+            TestItem.LastName = "";
+            TestItem.Password = "Something!";
+            TestItem.PhoneNumber = "02345 675243";
+            TestItem.FirstName = "John";
+            TestItem.JobTitle = "Clerk";
+            //add the item to the list
+            mStaffList.Add(TestItem);
+
         }
 
         //public List<clsStaff> StaffList { get; set; }
--- Toys4U Testing/tstStaffCollection.cs
+++ Toys4U Testing/tstStaffCollection.cs
@@ -57,7 +57,7 @@ namespace Toys4U_Testing
             //create an instance of the class we want to create 
             clsStaffCollection AllStaff = new clsStaffCollection();
             //create some test data to assign to the property 
-            Int32 SomeCount = 0;
+            Int32 SomeCount = 2;
             //assign the data to the property
             AllStaff.Count = SomeCount;
             //test to see that the two vlauyes are the same 
@@ -118,6 +118,16 @@ namespace Toys4U_Testing
             AllStaff.StaffList = TestList;
             //test to see that the two values are tghe same
             Assert.AreEqual(AllStaff.Count, TestList.Count);
+        }
+        
+        [TestMethod]
+        public void TwoRecordsPresent()
+        {
+            //create an instance of the class we want to create
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            //test to see that the two values ar ethe sama , aka there is two records present
+            Assert.AreEqual(AllStaff.Count, 2);
+
         }
     }
 
