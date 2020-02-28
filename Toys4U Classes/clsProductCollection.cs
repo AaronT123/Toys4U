7bb5e221231cd34764fb2bf5757c607857445d01
Anil Dosanjh <P17166483@my365.dmu.ac.uk>
2020-02-28 22:59:03 +0000

    almost completed week 20. 1/4 left.

--- Toys4U Classes/clsProductCollection.cs
+++ Toys4U Classes/clsProductCollection.cs
@@ -4,6 +4,72 @@ namespace Toys4U_Classes
 {
     public class clsProductCollection
     {
-        public List<clsProduct> ProductList { get; set; }
+        //private data member for the list
+        List<clsProduct> mProductList = new List<clsProduct>();
+
+        //contructor for the class
+        public clsProductCollection()
+        {
+            //create the items of test data 
+            clsProduct TestItem = new clsProduct();
+            //set it properties
+            TestItem.ProductID = 1;
+            TestItem.Name = "Yellow Duck";
+            TestItem.Description = "toddler toy";
+            TestItem.StockQuantity = 4;
+            //add the item to the test list 
+            mProductList.Add(TestItem);
+            // re intialise the object for some new data 
+            TestItem = new clsProduct();
+            //set its properties 
+            TestItem.ProductID = 2;
+            TestItem.Name = "doll";
+            TestItem.Description = "baby doll";
+            TestItem.StockQuantity = 2;
+            //add the item to the test list
+            mProductList.Add(TestItem);
+        }
+
+        //public property for the product list
+        public List<clsProduct> ProductList
+        {
+            get
+            {
+                //return the private data
+                return mProductList;
+            }
+            set
+            {
+                //set the private data
+                mProductList = value;
+            }
+        }
+
+        //public propert for count
+        public int Count
+        {
+            get
+            {
+                //return the count list
+                return mProductList.Count;
+            }
+            set
+            {
+                //worry about this later
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        public clsProduct ThisProduct { get; set; }
     }
 }
--- Toys4U Testing/tstProductCollection.cs
+++ Toys4U Testing/tstProductCollection.cs
@@ -41,5 +41,73 @@ namespace Toys4U_Testing
             Assert.AreEqual(AllProduct.ProductList, TestList);
 
         }
+
+        [TestMethod]
+        public void CountPropertyOK()
+        {
+            //create an instance of the class we want to create           
+            clsProductCollection AllProduct = new clsProductCollection();
+            //create some test data
+            Int32 SomeCount = 2;
+            //assign data to property
+            AllProduct.Count = SomeCount;
+            //test to see that the two value are the same
+            Assert.AreEqual(AllProduct.Count, SomeCount);            
+        }
+
+        [TestMethod]
+        public void ThisProductPropertyOK()
+        {
+            //create an instance of the class we want to create 
+            clsProductCollection AllProduct = new clsProductCollection();
+            //create some test data to assign to the property 
+            clsProduct TestProduct = new clsProduct();
+            //set the properties of the test object
+            TestProduct.ProductID = 2;
+            TestProduct.Name = "Car Toy";
+            TestProduct.Description = "New car toy";
+            TestProduct.StockQuantity = 3;
+            //assign the data to the property 
+            AllProduct.ThisProduct = TestProduct;
+            //test to see that the two value are the same
+            Assert.AreEqual(AllProduct.ThisProduct, TestProduct);
+        }
+
+        //test if records can be added to the lists
+        //and the Count property correctly reports how many items are in the list.
+
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //CREATE an instance of the class we want to create 
+            clsProductCollection AllProduct = new clsProductCollection();
+            //create some test data to assign to the property 
+            //in this case the data needs to be a list of objects 
+            List<clsProduct> TestList = new List<clsProduct>();
+            //add an item to the list 
+            //create some test data of the item
+            clsProduct TestItem = new clsProduct();
+            //set its properties 
+            TestItem.ProductID = 4;
+            TestItem.Name = "Hot Wheels";
+            TestItem.Description = "Fast and Furious season";
+            TestItem.StockQuantity = 20;
+            //add the test item to the list 
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllProduct.ProductList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllProduct.Count, TestList.Count);
+        }
+
+        [TestMethod]
+        public void TwoRecordsPresent()
+        {
+            //create an instance of the class we want to create 
+            clsProductCollection AllProduct = new clsProductCollection();
+            //test to see that two values are the same 
+            Assert.AreEqual(AllProduct.Count, 2);
+        }
     }
 }
