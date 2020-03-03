b6bc57be80eb9567ba1f905344db6b5aae95806b
p17166483 <p17166483@GWHXX473WIN015W>
2020-03-03 16:43:15 +0000

    listbox

---
+++ Toys4U Front Office/ProductHome.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ProductHome.aspx.cs" Inherits="Toys4U_Front_Office.ProductHome" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #form1 {
+            height: 9px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:ListBox ID="LstToys" runat="server" Height="200px" Width="384px"></asp:ListBox>
+        <br />
+        <br />
+        <p>
+            <asp:TextBox ID="TxtToys" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Button ID="BtnApply" runat="server" Height="53px" OnClick="BtnApply_Click" Text="Apply" Width="78px" />
+        </p>
+        <p>
+            <asp:Button ID="BtnDisplayAll" runat="server" Height="57px" OnClick="Button2_Click" Text="Display All" Width="80px" />
+        </p>
+        <asp:Button ID="BtnAdd" runat="server" Text="Add" />
+        <asp:Button ID="BtnEdit" runat="server" Text="Edit" />
+        <asp:Button ID="BtnDelete" runat="server" OnClick="BtnDelete_Click" Text="Delete" />
+    </form>
+</body>
+</html>
---
+++ Toys4U Front Office/ProductHome.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Toys4U_Front_Office
+{
+    public partial class ProductHome : System.Web.UI.Page
+    {
+        //this function handles the load event for the page
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if this is the first time the page is displayed
+            if (IsPostBack == false)
+            {
+                //update the list box
+                DisplayToys();
+            }
+        }
+
+        void DisplayToys()
+        {
+            //create an instance of the toys collection
+            Toys4U_Classes.clsProductCollection Toys = new Toys4U_Classes.clsProductCollection();
+            //set the data source to the list of toys in  the collection
+            LstToys.DataSource = Toys.ProductList;
+            //set the name of the primary key
+            LstToys.DataValueField = "ProductID";
+            //set the data feild to display
+            LstToys.DataTextField = "Name";
+            //set the data feild to display
+            LstToys.DataTextField = "Description";
+            //set the data feild to display
+            LstToys.DataTextField = "StockQuantity";
+            //bind the data to the list
+            LstToys.DataBind();
+
+        }
+
+        protected void BtnApply_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
